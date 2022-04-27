86b75f62a482d741038f716eb2f47206b4612c9f
Dhan Ortiz <dhan.marlan.ortiz@gmail.com>
2022-04-27 10:01:09 +0800

    removed monolog

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '0fb18dbbeaaeeadb17e290be48db2702936b56cd',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '0fb18dbbeaaeeadb17e290be48db2702936b56cd',
             'dev_requirement' => false,
         ),
         'monolog/monolog' => array(
