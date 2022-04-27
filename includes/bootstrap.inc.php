49284fe4712ee43566e184160a131664e4d35deb
Dhan Ortiz <dhan.marlan.ortiz@gmail.com>
2022-04-27 09:08:18 +0800

    bug fixes

---
+++ includes/bootstrap.inc.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('vendor/autoload.php');
+    
+    require_once('lib/CommentParser.php');
+    require_once('lib/Modifier.php');
+    require_once('lib/Parameter.php');
+    require_once('lib/Parser.php');
+    require_once('lib/Section.php');
+    
+    
+    $kss = new Kss\Parser('css');
+?>
